4b9d18512fc64fa77eeb2d084424ae260e5ad6a0
py_gilfoil <78735996+Tiesto95@users.noreply.github.com>
2026-03-15 03:06:35 +0300

    Update [...path].js

--- api/proxy/[...path].js
+++ api/proxy/[...path].js
@@ -2,44 +2,69 @@ export const config = {
   runtime: 'edge',
 };
 
+const PROXY_BASE = '/api/proxy';
+
+const HOSTS = {
+  '/html5api': 'html5.api.gamedistribution.com',
+  '/gameapi':  'game.api.gamedistribution.com',
+  '/imgapi':   'img.gamedistribution.com',
+  '/pmapi':    'pm.gamedistribution.com',
+};
+
+const DEFAULT_HOST = 'html5.gamedistribution.com';
+
+function getTarget(fullPath) {
+  for (const [prefix, host] of Object.entries(HOSTS)) {
+    if (fullPath.startsWith(prefix)) {
+      return { host, cleanPath: fullPath.slice(prefix.length) || '/' };
+    }
+  }
+  return { host: DEFAULT_HOST, cleanPath: fullPath || '/' };
+}
+
+function rewriteBody(body, proxyBase) {
+  const replacements = [
+    ['https://html5.api.gamedistribution.com', proxyBase + '/html5api'],
+    ['http://html5.api.gamedistribution.com',  proxyBase + '/html5api'],
+    ['//html5.api.gamedistribution.com',       proxyBase + '/html5api'],
+    ['https://game.api.gamedistribution.com',  proxyBase + '/gameapi'],
+    ['http://game.api.gamedistribution.com',   proxyBase + '/gameapi'],
+    ['//game.api.gamedistribution.com',        proxyBase + '/gameapi'],
+    ['https://img.gamedistribution.com',       proxyBase + '/imgapi'],
+    ['http://img.gamedistribution.com',        proxyBase + '/imgapi'],
+    ['//img.gamedistribution.com',             proxyBase + '/imgapi'],
+    ['https://pm.gamedistribution.com',        proxyBase + '/pmapi'],
+    ['http://pm.gamedistribution.com',         proxyBase + '/pmapi'],
+    ['//pm.gamedistribution.com',              proxyBase + '/pmapi'],
+    ['https://html5.gamedistribution.com',     proxyBase],
+    ['http://html5.gamedistribution.com',      proxyBase],
+    ['//html5.gamedistribution.com',           proxyBase],
+  ];
+  for (const [from, to] of replacements) {
+    body = body.split(from).join(to);
+  }
+  return body;
+}
+
 export default async function handler(req) {
   const url = new URL(req.url);
-  const fullPath = url.pathname.replace('/api/proxy', '') || '/';
+  const fullPath = url.pathname.replace(PROXY_BASE, '') || '/';
   const search = url.search || '';
 
-  // Определяем нужный поддомен по префиксу пути
-  let targetHost = 'html5.gamedistribution.com';
-  let cleanPath = fullPath;
-
-  if (fullPath.startsWith('/html5api')) {
-    targetHost = 'html5.api.gamedistribution.com';
-    cleanPath = fullPath.replace('/html5api', '') || '/';
-  } else if (fullPath.startsWith('/gameapi')) {
-    targetHost = 'game.api.gamedistribution.com';
-    cleanPath = fullPath.replace('/gameapi', '') || '/';
-  } else if (fullPath.startsWith('/imgapi')) {
-    targetHost = 'img.gamedistribution.com';
-    cleanPath = fullPath.replace('/imgapi', '') || '/';
-  } else if (fullPath.startsWith('/pmapi')) {
-    targetHost = 'pm.gamedistribution.com';
-    cleanPath = fullPath.replace('/pmapi', '') || '/';
-  }
-
-  const targetUrl = `https://${targetHost}${cleanPath}${search}`;
+  const { host, cleanPath } = getTarget(fullPath);
+  const targetUrl = 'https://' + host + cleanPath + search;
 
   const response = await fetch(targetUrl, {
     method: req.method,
     headers: {
       'User-Agent': 'Mozilla/5.0',
       'Accept': '*/*',
-      'Accept-Language': 'en-US,en;q=0.9',
-      'Referer': `https://${targetHost}/`,
+      'Referer': 'https://' + host + '/',
     },
   });
 
   const contentType = response.headers.get('content-type') || '';
   const headers = new Headers(response.headers);
-
   headers.delete('X-Frame-Options');
   headers.delete('x-frame-options');
   headers.delete('Content-Security-Policy');
@@ -47,22 +72,11 @@ export default async function handler(req) {
   headers.set('Access-Control-Allow-Origin', '*');
 
   if (contentType.includes('text/html') || contentType.includes('javascript')) {
-    let body = await response.text();
-    const origin = url.origin;
-    const proxyBase = origin + '/api/proxy';
+    const body = await response.text();
+    const proxyBase = url.origin + PROXY_BASE;
+    const rewritten = rewriteBody(body, proxyBase);
+    return new Response(rewritten, { status: response.status, headers });
+  }
 
-    body = body
-      .replaceAll('https://html5.api.gamedistribution.com', proxyBase + '/html5api')
-      .replaceAll('http://html5.api.gamedistribution.com', proxyBase + '/html5api')
-      .replaceAll('//html5.api.gamedistribution.com', proxyBase + '/html5api')
-      .replaceAll('https://game.api.gamedistribution.com', proxyBase + '/gameapi')
-      .replaceAll('http://game.api.gamedistribution.com', proxyBase + '/gameapi')
-      .replaceAll('//game.api.gamedistribution.com', proxyBase + '/gameapi')
-      .replaceAll('https://img.gamedistribution.com', proxyBase + '/imgapi')
-      .replaceAll('http://img.gamedistribution.com', proxyBase + '/imgapi')
-      .replaceAll('//img.gamedistribution.com', proxyBase + '/imgapi')
-      .replaceAll('https://pm.gamedistribution.com', proxyBase + '/pmapi')
-      .replaceAll('http://pm.gamedistribution.com', proxyBase + '/pmapi')
-      .replaceAll('//pm.gamedistribution.com', proxyBase + '/pmapi')
-      .replaceAll('https://html5.gamedistribution.com', proxyBase)
-      .replaceAll('http://html5.gamedistri
+  return new Response(response.body, { status: response.status, headers });
+}
